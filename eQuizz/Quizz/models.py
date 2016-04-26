3e6bf26c45319ae6c35f3f6bd7fa11863ad04590
Nicolas Lanore <nicolas.lanore@gmail.com>
2016-04-26 13:05:06 +0200

    Modification des modèles

--- eQuizz/Quizz/models.py
+++ eQuizz/Quizz/models.py
@@ -4,37 +4,33 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-
+"""Une séance représente une séance de cours.
+Elle est crée par l'enseignant.
+Elle contient plusieurs questions."""
 class Seance(models.Model):
     #titre = models.CharField(max_length=100)
     #contenu = models.TextField(null=True)
-    #date = models.DateTimeField(auto_now_add=True, auto_now=False, 
+    #date = models.DateTimeField(auto_now_add=True, auto_now=False,
     #                            verbose_name="Date de parution")
     id = models.AutoField(primary_key = True)
     code = models.IntegerField(unique = True) #check for lengthpython
 
-    
+
     def __str__(self):
-        """ 
-        Cette méthode que nous définirons dans tous les modèles
-        nous permettra de reconnaître facilement les différents objets que 
-        nous traiterons plus tard et dans l'administration
-        """
         return self.id
 
+"""Une réponse correspond à un choix A, B, C ou D d'un élève à un question."""
 class Answers(models.Model):
-    answer_id = models.AutoField(primary_key = True)
-    question_id = models.ForeignKey('Question')
-    answer = models.TextField(null = True)
+    question = models.ForeignKey('Question')
+    answer = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return self.answer_id
-		
+
+"""Une question appartient à une séance. Elle va posséder plusieurs réponses."""
 class Question(models.Model):
-	quesion_id = models.AutoField(primary_key = True)
-	code=models.ForeignKey('Seance')
+	seance = models.ForeignKey('Seance')
 	commentaire = models.TextField(null = True)
+
 	def __str__(self):
 		return self.question_id
-
